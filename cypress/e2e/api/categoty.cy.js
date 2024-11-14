119e66ada752b345bef13b312e6e44f02c8ccc58
Quintiliano Paquini Nery <quintiliano1995@gmail.com>
2024-11-14 19:55:58 -0300

    Melhorias no testes de Categorias

--- cypress/e2e/api/categoty.cy.js
+++ cypress/e2e/api/categoty.cy.js
@@ -1,8 +1,9 @@
 const users = require('../../fixtures/web/data/users.json')
-import { postUserAndToken } from '../functions/auth.js'
-import { getIdCategory } from '../functions/getIdCategory.js'
-
+import { postUserAndToken } from '../../fixtures/functions/auth.js'
+import { getIdCategory, postNewCategory } from '../../fixtures/functions/category.js'
 const faker = require('faker-br')
+
+const image = "https://www.hostinger.com.br/tutoriais/wp-content/uploads/sites/12/2019/12/O-Que-e-WebP-Um-Guia-para-Iniciantes.webp"
 const randomCategoryName = faker.commerce.department()
 
 describe('Categorias', () => {
@@ -22,55 +23,55 @@ describe('Categorias', () => {
             },
             body: {
                 "name": randomCategoryName,
-                "photo": "https://www.hostinger.com.br/tutoriais/wp-content/uploads/sites/12/2019/12/O-Que-e-WebP-Um-Guia-para-Iniciantes.webp"
+                "photo": image
             }
         }).then((res) => {
-            expect(res.status).to.eq(200);
-            expect(res.body).to.have.property('success').to.eq(true);
+            cy.validateParametersResponse(res.status, res.body, 200, true)
             expect(res.body).to.have.property('message').to.eq('category added');
             expect(res.body.data).to.have.property('name').to.eq(randomCategoryName);
         });
     });
 
     it('Listar categorias', () => {
+        postNewCategory(randomCategoryName, image)
         cy.request({
             method: 'GET',
             url: '/public/getCategories',
         }).then((res) => {
-            expect(res.status).to.eq(200);
-            expect(res.body).to.have.property('success').to.eq(true);
+            cy.validateParametersResponse(res.status, res.body, 200, true)
             expect(res.body.categories[0]._id).to.exist;
         });
     });
 
     it('Alterar categoria', () => {
+        cy.log(Cypress.env('categoryId'))
+        postNewCategory(randomCategoryName, image)
         cy.request({
             method: 'PUT',
-            url: `/api/editCategory/${Cypress.env('_id')}`,
+            url: `/api/editCategory/${Cypress.env('categoryId')}`,
             headers: {
                 'Authorization': Cypress.env('token')
             },
             body: {
                 "name": randomCategoryName,
-                "photo": "https://www.hostinger.com.br/tutoriais/wp-content/uploads/sites/12/2019/12/O-Que-e-WebP-Um-Guia-para-Iniciantes.webp"
+                "photo": image
             }
         }).then((res) => {
-            expect(res.status).to.eq(200);
-            expect(res.body).to.have.property('success').to.eq(true);
+            cy.validateParametersResponse(res.status, res.body, 200, true)
             expect(res.body).to.have.property('message').to.eq('category updated');
         });
     });
 
     it('Excluir categoria', () => {
+        postNewCategory(randomCategoryName, image)
         cy.request({
             method: 'DELETE',
-            url: `/api/deleteCategory/${Cypress.env('_id')}`,
+            url: `/api/deleteCategory/${Cypress.env('categoryId')}`,
             body: {
                 "authorization": Cypress.env('token')
             }
         }).then((res) => {
-            expect(res.status).to.eq(200);
-            expect(res.body).to.have.property('success').to.eq(true);
+            cy.validateParametersResponse(res.status, res.body, 200, true)
             expect(res.body).to.have.property('message').to.eq('category deleted');
         });
     });
---
+++ cypress/fixtures/functions/category.js
@@ -0,0 +1,24 @@
+export const getIdCategory = () => {
+    cy.request({
+        method: 'GET',
+        url: '/public/getCategories',
+    }).then((res) => {
+        let categoryId = res.body.categories[0]._id;
+        Cypress.env('categoryId', categoryId); // Armazena o _id em Cypress.env
+        return categoryId;
+    });
+}
+
+export const postNewCategory = (randomCategoryName, image) => {
+    cy.request({
+        method: 'POST',
+        url: '/api/addCategory',
+        headers: {
+            'Authorization': Cypress.env('token')
+        },
+        body: {
+            "name": randomCategoryName,
+            "photo": image
+        }
+    })
+}
